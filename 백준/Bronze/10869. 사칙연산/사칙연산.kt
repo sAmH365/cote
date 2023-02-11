ebae770630edca2b848fa3c5fd53415a7a0c54dd
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-02-11 14:00:28 +0900

    [Bronze V] Title: 사칙연산, Time: 156 ms, Memory: 17124 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val input = sc.nextLine().split(" ").map { it.toInt() }
+    sc.close()
+
+    println(input[0] + input[1])
+    println(input[0] - input[1])
+    println(input[0] * input[1])
+    println(input[0] / input[1])
+    println(input[0] % input[1])
+}
