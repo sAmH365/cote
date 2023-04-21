c37d56607c2182cbabc2a52bd85e20450a8a3c8b
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-04-21 10:43:07 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 492 ms, Memory: 104748 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.kt
@@ -0,0 +1,33 @@
+import Main.Companion.count
+import Main.Companion.sb
+import java.lang.StringBuilder
+import java.util.Scanner
+
+class Main {
+    companion object {
+        var count = 0
+        var sb = StringBuilder()
+    }
+}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val input = sc.nextInt()
+    sc.close()
+
+    hanoi(input, 1, 2, 3)
+    sb.insert(0, "${count}\n")
+    println(sb)
+}
+
+fun hanoi(n: Int, from: Int, by: Int, to: Int) {
+    if (n == 1) {
+        ++count
+        sb.append("$from ${to}\n")
+    } else {
+        ++count
+        hanoi(n - 1, from, to, by)
+        sb.append("$from ${to}\n")
+        hanoi(n - 1, by, from, to)
+    }
+}
