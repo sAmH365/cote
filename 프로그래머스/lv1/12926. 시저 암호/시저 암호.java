6ded5b98235f7c9650ccf8d6234d1ff5602abfb0
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-05-15 17:41:35 +0900

    [level 1] Title: 시저 암호, Time: 32.31 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,31 +1,27 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
-        char[] chars = s.toCharArray();
-        char[] tmp = new char[chars.length];
 
-        // A~Z 65 ~ 90
-        // a~z 97 ~ 122
-        for (int i=0; i< chars.length; i++) {
-            int cal = chars[i] + n;
-
-            if (chars[i] != ' ') {
-                if (chars[i] >= 65 && chars[i] <= 90) {
-                    if (cal > 90) cal -= 26;
-                } else if (chars[i] >= 97 && chars[i] <= 122) {
-                    if (cal > 122) cal -= 26;
-                }
-                tmp[i] = (char) cal;
+        for (int i=0; i< s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if (Character.isUpperCase(ch)) {
+                ch += n;
+                
+                if (ch > 'Z') ch -= 26;
+                
+                answer += (ch+"");
+            } else if (Character.isLowerCase(ch)) {
+                ch += n;
+                
+                if (ch > 'z') ch -= 26;
+                answer += (ch+"");
+            } else {
+                answer += " ";
             }
         }
         
-        for (int i=0; i < tmp.length; i++) {
-            if (tmp[i] == '\u0000') {
-                answer += " ";
-            } else {
-                answer += (tmp[i]+"");
-            }
-        }
+        // String answer = new String(tmp);
         
         return answer;
     }
