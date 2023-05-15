3960cdd32f053ed01c90d326e9b81151ef8a84ee
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-05-16 02:00:38 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 6.65 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] strs = s.split("");
+        String[] splitS = s.split(" ");
+        
+        boolean isWord = false;
+        
+        int j = 0;
+        
+        ArrayList<String> arr = new ArrayList();
+        
+        for (int i=0; i<strs.length; i++) {
+            if (strs[i].equals(" ")) {
+                isWord = false;
+                arr.add(strs[i]);
+            } else {
+                isWord=true;
+            }
+            
+            if (isWord) {
+                if (j % 2 == 0) {
+                    arr.add(Character.toUpperCase(strs[i].charAt(0)) + "");
+                    j++;
+                } else {
+                    arr.add(Character.toLowerCase(strs[i].charAt(0)) + "");
+                    j++;
+                }
+            } else {
+                j = 0;
+            }
+        }
+        
+        for (String el : arr) {
+            answer += el;
+        }
+        return answer;
+    }
+}
