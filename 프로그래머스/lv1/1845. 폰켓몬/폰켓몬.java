ad0decd3ef9913375f9d70bd9e7c03362b1e5bdb
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-06-22 13:15:36 +0900

    [level 1] Title: 폰켓몬, Time: 2.41 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -3,18 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        
+        // 중복 제거 -> 해시
+        // 최대 개수 = nums.length / 2
         int max = nums.length / 2;
-        HashSet set = new HashSet();
+        HashMap<Integer, Integer> map = new HashMap();
         
+        // 해시 테이블에 포켓몬 집어넣기  [1, 2, 3]
         for (int num : nums) {
-            set.add(num);
+            map.put(num, num);
         }
-        
-        if (set.size() <= max) {
-            answer = set.size();
-        } else {
-            answer = max;
+
+        // 최대 개수랑 비교하면서 answer수 증가
+        for (Integer key : map.keySet()) {
+            if (answer >= max) break;
+            answer++;
         }
         
         return answer;
