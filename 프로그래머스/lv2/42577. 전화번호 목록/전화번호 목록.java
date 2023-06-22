8794eb2fc2780678b0af3805521cc9feed08326f
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-06-22 14:45:54 +0900

    [level 2] Title: 전화번호 목록, Time: 296.12 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -6,13 +6,12 @@ class Solution {
         
         Arrays.sort(phone_book);
         
-        for (int i=0; i<phone_book.length-1;i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
-                answer = false;
-                break;
+        for (int i=0; i<phone_book.length; i++) {
+            for (int j=i+1; j<phone_book.length; j++) {
+                if (phone_book[j].startsWith(phone_book[i])) return false;
+                else break;
             }
         }
-        
         return answer;
     }
 }
