a4b89e5102845bfc0bdf3aac26aeefed8e507178
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-06-23 15:23:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 88.69 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 118 MB, 시간: 26.94 ms
+메모리: 116 MB, 시간: 88.69 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,22 +1,21 @@
-import java.util.ArrayList;
-
+import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-
-        ArrayList<Integer> ar = new ArrayList();
+        int[] answer = {};
         
-        ar.add(arr[0]);
+        Stack<Integer> stack = new Stack();
         
-        for (int i=1; i< arr.length; i++) {
-            if (!(arr[i] == arr[i-1])) {
-                ar.add(arr[i]);
-            }
+        for (int i=0; i<arr.length; i++) {
+            if (stack.size() == 0) stack.push(arr[i]);
+            if (stack.peek() != arr[i]) stack.push(arr[i]);
         }
         
-        int[] answer = new int[ar.size()];
-        for (int i=0; i<ar.size();i++) {
-            answer[i] = ar.get(i);
+        answer = new int[stack.size()];
+        int i=0;
+        
+        while(stack.size() != 0) {
+            answer[stack.size()-1] = stack.pop();
         }
 
         return answer;
