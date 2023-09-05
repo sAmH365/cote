0a6438d9b8124504f6ca1f430efc7fc61389f28c
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2023-09-05 18:28:23 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String input = bf.readLine();
+        
+        System.out.println(input+"??!");
+    }
+}
