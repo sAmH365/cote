085882670b0f9487bad58b283c4a9aee5593cc65
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2025-06-17 12:32:44 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 15464 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,31 +1,44 @@
-
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int line = Integer.parseInt(bf.readLine());
-        Stack<String> stack = new Stack<>();
-        String[] result = new String[line];
-        int count = 0;
 
-        for (int i=0; i<line; i++) {
-            String[] ps = bf.readLine().split("");
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(bf.readLine());
 
-            for (int j=0; j<ps.length; j++) {
-                if (ps[j].equals("(")) {
-                    count++;
-                } else if (ps[j].equals(")") && count == 0) {
-                    count--;
-                    break;
-                } else count--;
-            }
-
-            if (count != 0) System.out.println("NO");
-            else System.out.println("YES");
-
-            count = 0;
-        }
+    String inputStrs[] = new String[num];
+    for (int i = 0; i<num; i++) {
+      StringTokenizer st = new StringTokenizer(bf.readLine());
+      inputStrs[i] = st.nextToken();
     }
+
+    for (String inputStr : inputStrs) {
+      System.out.println(solve(inputStr));
+    }
+  }
+
+  public static String solve(String s) {
+    Stack<String> stack = new Stack<>();
+    String[] splits = s.split("");
+
+    for (String split : splits) {
+      if (split.equals("(")) {
+        stack.push(split);
+      } else if (stack.empty()) {
+        return "NO";
+      } else {
+        stack.pop();
+      }
+    }
+
+    if (stack.empty()) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+  }
 }
