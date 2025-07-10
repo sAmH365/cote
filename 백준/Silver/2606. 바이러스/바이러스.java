60745d9f8e9e6a3c41882a17e3d0934ebcddba82
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2025-07-10 10:59:09 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,47 +1,47 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] graph;
-    static boolean[] visited;
-    static int N, M;     
-    static int count;
+  static boolean[][] graph;
+  static boolean [] visited;
+  static int N, M;
+  static int count;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+    N = Integer.parseInt(bf.readLine());
+    M = Integer.parseInt(bf.readLine());
 
-        graph = new boolean[N + 1][N + 1];
-        visited = new boolean[N + 1];
+    graph = new boolean[N + 1][N + 1];
+    visited = new boolean[N + 1];
 
-        int x, y;
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
+    int x,y;
+    for (int i=0; i<M; i++) {
+      StringTokenizer st = new StringTokenizer(bf.readLine());
 
-            graph[x][y] = true;     
-            graph[y][x] = true;
-        }
+      x = Integer.parseInt(st.nextToken());
+      y = Integer.parseInt(st.nextToken());
 
-        dfs(1);
-        bw.write(String.valueOf(count - 1));
-        
-        br.close();
-        bw.close();
+      graph[x][y] = true;
+      graph[y][x] = true;
     }
 
-    static void dfs(int idx) {
-        visited[idx] = true;
-        count++;
+    dfs(1);
 
-        for (int i = 1; i <= N; i++) {
-            if (visited[i] == false && graph[idx][i]) {     
-                dfs(i);
-            }
-        }
+    System.out.println(count - 1);
+  }
+
+  static void dfs(int idx) {
+    visited[idx] = true;
+    count++;
+
+    for (int i=1; i <= N; i++) {
+      if (!visited[i] && graph[idx][i]) {
+        dfs(i);
+      }
     }
+  }
 }
