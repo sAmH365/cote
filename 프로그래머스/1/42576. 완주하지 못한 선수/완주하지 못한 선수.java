ae3c430b354401eed3c096e6f0d1bcea1bfce3d2
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2025-11-30 15:55:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 109.26 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String part: participant) {
+            map.put(part, map.getOrDefault(part, 0) + 1);
+        }
+        
+        for (String comp: completion) {
+            map.put(comp, map.get(comp) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+            }
+        }
+        
+        return answer;
+    }
+}
