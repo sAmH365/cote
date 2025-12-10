2be524317bd4919670fc05d501af31e89516cb4c
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2025-12-10 18:42:45 +0900

    [level 1] Title: 시저 암호, Time: 0.83 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,6 +1,6 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 10일 18:42:15
+2025년 12월 10일 18:42:41
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,24 +1,26 @@
 import java.util.*;
 
-    class Solution {
-      public String solution(String s, int n) {
+class Solution {
+    public String solution(String s, int n) {
+        String[] strs = s.split("");
         StringBuilder sb = new StringBuilder();
-
-        for (char c : s.toCharArray()) {
-          if (c == ' ') {
-            sb.append(' ');
+        
+        for (int i = 0; i < strs.length; i++) {
+          if (strs[i].equals(" ")) {
+            sb.append(" ");
             continue;
           }
+          char c = (char)(strs[i].charAt(0) + n);
 
-          if (c >= 'a' && c <= 'z') {
-            c = (char)((c - 'a' + n) % 26 + 'a');
-          } else if (c >= 'A' && c <= 'Z') {
-            c = (char)((c - 'A' + n) % 26 + 'A');
+          if ((c > 'Z' && c < 'a') || c > 'z') {
+            c = (char)(c - ('z'-'a'+1));
           }
 
           sb.append(c);
         }
-
-        return sb.toString();
-      }
+        
+        
+        String answer = sb.toString();
+        return answer;
     }
+}
