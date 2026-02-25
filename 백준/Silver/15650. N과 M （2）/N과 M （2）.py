c7756b45435cf837f61ee3933ff2945e751b76ab
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2026-02-25 14:14:24 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -4,17 +4,16 @@ input = sys.stdin.readline
 
 N, M = list(map(int, input().split()))
 
-arr_input = [v for v in range(1, N + 1)]
+input_arr = [v for v in range(1, N + 1)]
 
-result = []
 
-def solve(start: int, current_arr: list):
+def solve(index: int, current_arr: list):
   if len(current_arr) == M:
     print(*current_arr)
     return
 
-  for i in range(start, N):
-    current_arr.append(arr_input[i])
+  for i in range(index, len(input_arr)):
+    current_arr.append(input_arr[i])
     solve(i + 1, current_arr)
     current_arr.pop()
 
