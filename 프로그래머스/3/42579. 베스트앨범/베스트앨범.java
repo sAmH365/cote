47928b9bc14f70a89483f45c07fa38c318dc9596
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2026-03-18 09:55:21 +0900

    [level 3] Title: 베스트앨범, Time: 1.65 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,81 +2,81 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        
-        List<Integer> result = new ArrayList<>();
-        
-        Map<String, List<Song>> songMap = new HashMap();
-        Map<String, Integer> totalSongCountMap = new HashMap();
+        List<Integer> answer = new ArrayList();
+
+        Map<String, List<Music>> genreMap = new HashMap();
         for (int i = 0; i < genres.length; i++) {
             String key = genres[i];
-            Song song = new Song(i, plays[i]);
+            List<Music> genreList = genreMap.get(key);
+            if (genreList == null) genreList = new ArrayList();
             
-            List<Song> songs = songMap.get(key);
-            if (songs == null) {
-                songs = new ArrayList();
+            genreList.add(new Music(key, i, plays[i]));
+            genreMap.put(key, genreList);
+        }
+        
+        // 장르별 재생횟수총합 구하기
+        Map<String, Integer> totalCountMap = new HashMap();
+        for (String key : genreMap.keySet()) {
+            List<Music> musics = genreMap.get(key);
+            int count = 0;
+            for (Music m : musics) {
+                count += m.getPlayCount();
             }
-            songs.add(song);
-            
-            totalSongCountMap.put(
-                key,
-                totalSongCountMap.getOrDefault(key, 0) + plays[i]
-            );
-            songMap.put(key, songs);
+            totalCountMap.put(key, count);
         }
         
-        List<String> genreKeys = new ArrayList<>(totalSongCountMap.keySet());
-        for (String g : genreKeys) {
-            System.out.print(g + " ");
-        }
-        System.out.println();
+        // 많이 재생된 장르 구하기
+        List<String> keySet = new ArrayList(totalCountMap.keySet());
+        keySet.sort((o1, o2) -> totalCountMap.get(o2).compareTo(totalCountMap.get(o1)));
         
-        genreKeys.sort((g1, g2) -> {
-         return totalSongCountMap.get(g2) - totalSongCountMap.get(g1);   
-        });
-        
-        int tmpCount = 0;
-        for(String g : genreKeys) {
-            List<Song> songs = songMap.get(g);
-            songs.sort((s1, s2) -> {
-                return s2.getCount() - s1.getCount();
-            });
+        // 해당 장르 내에서 플레이 횟수 내림차순 정렬
+        // 두곡 수록 (한곡 만 있을경우 한개만 수록)
+        for (String k : keySet) {
+            List<Music> musics = genreMap.get(k);
             
-            if (songs.size() >= 2) {
-                result.add(songs.get(0).getId());
-                result.add(songs.get(1).getId());    
+            musics.sort((o1, o2) -> o2.getPlayCount() - o1.getPlayCount());
+            
+            if (musics.size() == 1) {
+                answer.add(musics.get(0).getIdx());
             } else {
-                result.add(songs.get(0).getId());
+                answer.add(musics.get(0).getIdx());
+                answer.add(musics.get(1).getIdx());
             }
-            
         }
         
-        int[] answer = new int[result.size()];
-        
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
+        int[] result = new int[answer.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = answer.get(i);
         }
         
-        return answer;
+        return result;
     }
     
- class Song {
-     private int id;
-     private int count;
-     
-     public Song(int id, int count) {
-         this.id = id;
-         this.count = count;
-     }
-     
-     public String toString() {
-         return String.format("id : %d, count : %d", id, count);
-     }
-     
-     public int getId() {
-         return this.id;
-     }
-     public int getCount() {
-         return this.count;
-     }
- }
+    class Music {
+        private String genre;
+        private int idx;
+        private int playCount;
+        
+        public Music(String genre, int idx, int playCount) {
+            this.genre = genre;
+            this.idx = idx;
+            this.playCount = playCount;
+        }
+        
+        public String toString() {
+            return String.format("genre: %s, idx: %d, cnt: %d", genre, idx, playCount);
+        }
+        
+        public String getGenre() {
+            return genre;
+        }
+        
+        public int getIdx() {
+            return idx;
+        }
+        
+        public int getPlayCount() {
+            return playCount;
+        }
+    }
 }
