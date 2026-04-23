d3e20e292ccc6843d6b53682c377425ebe0b18d5
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2026-04-24 08:41:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.04 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-    Map<String, Integer> partiMap = new HashMap<>();
-
-    for (String parti : participant) {
-      partiMap.put(parti, partiMap.getOrDefault(parti, 0) + 1);
-    }
-
-    for (String complete : completion) {
-      partiMap.put(complete, partiMap.get(complete) - 1);
-    }
-
-    for (String key : partiMap.keySet()) {
-      if (partiMap.get(key) != 0) {
-        return key;
-      }
-    }
-
-    return "";
+        String answer = "";
+        Map<String, Integer> map = new HashMap();
+        
+        // 1.참가자 기록
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        // 2.완주자와 비교하면서 숫자 차감
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }    
+        
+        // 3. 마지막 까지 남아있는 사람이 정답
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+        
+        return answer;
     }
 }
