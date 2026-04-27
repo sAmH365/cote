c15e6e38e8a933da098410145726affa9e8ef8d2
SaeHim Kwon <72846155+sAmH365@users.noreply.github.com>
2026-04-27 13:32:00 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,22 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-    int answer = 1;
-    Map<String, List<String>> clotheMap = new HashMap<>();
-
-    for (String[] clotheInfos : clothes) {
-      String category = clotheInfos[1];
-      String clothe = clotheInfos[0];
-
-      clotheMap.computeIfAbsent(category, k -> new ArrayList<>())
-          .add(clothe);
-    }
-
-    for (String key : clotheMap.keySet()) {
-      List<String> clothList = clotheMap.get(key);
-      answer *= clothList.size() + 1;
-    }
-
-    return answer - 1;
+        int answer = 1;
+        
+        Map<String, Integer> map = new HashMap();
+        for(String[] strs : clothes) {
+            String type = strs[1];
+            
+            // System.out.println(type);
+            
+            map.put(type, map.getOrDefault(type, 0) + 1);
+            
+            // System.out.println(map.get(type));
+        }
+        
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+        
+        return answer - 1;
     }
 }
